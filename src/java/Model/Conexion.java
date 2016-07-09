dbc3e7ea5eb07a4a42aee91c393f16a5be966326
unknown <edgar_mdz94@gmail.com>
2016-07-09 18:13:54 -0500

    Carpeta model!

--- src/java/Model/Conexion.java
+++ src/java/Model/Conexion.java
@@ -1,4 +1,4 @@
-package Modelo;
+package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class ConexionBD {
+public class Conexion {
 
     Connection connection = null;
 
